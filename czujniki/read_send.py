1414db30554286a0a2d33e8b34c27c57d916b3fc
bbbeton <jasiu.bloniarz@gmail.com>
2023-12-22 20:09:52 +0100

    Modified the read_send.py file

--- czujniki/read_send.py
+++ czujniki/read_send.py
@@ -14,24 +14,6 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-# Set up the GPIO mode
-GPIO.setmode(GPIO.BCM)
-
-# Set up the GPIO pin for reading the DO output
-DO_PIN = 7  # Replace with the actual GPIO pin number
-GPIO.setup(DO_PIN, GPIO.IN)
-
-def send_data(temperature, gas_state):
-    data = {"name": "Sensor", "value": temperature, "smoke_value": gas_state}
-    print(data)
-    response = requests.put(API_URL, data=json.dumps(data), headers={'Content-Type': 'application/json'})
-    print(response.status_code)
-    if response.status_code == 200 and response.content:
-        return response.json()
-    else:
-        print(f"Unexpected response: {response.status_code}, {response.content}")
-
-
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -49,17 +31,32 @@ def read_temp():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         return temp_c
-	
+
+# Set up the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Set up the GPIO pin for reading the DO output
+DO_PIN = 7 
+GPIO.setup(DO_PIN, GPIO.IN)
+
 def detect_gas():
     gas_present = GPIO.input(DO_PIN)
     if gas_present == GPIO.LOW:
         gas_state = 1
-        
     else:
         gas_state = 0
     return gas_state
 
-
+def send_data(temperature, gas_state):
+    data = {"name": "Sensor", "value": temperature, "smoke_value": gas_state}
+    print(data)
+    response = requests.put(API_URL, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    print(response.status_code)
+    if response.status_code == 200 and response.content:
+        return response.json()
+    else:
+        print(f"Unexpected response: {response.status_code}, {response.content}")
+        
 while True:
     temp_state = read_temp()
     gas_state = detect_gas()
